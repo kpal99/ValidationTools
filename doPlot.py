5df0ccb0d02c14f42f54fd07095b63167c94fcab
jmhogan <j.hogan@cern.ch>
2020-08-13 19:53:27 +0200

    TRatioPlot, photon tcls

--- doPlot.py
+++ doPlot.py
@@ -25,12 +25,19 @@ parser.add_option('-t','--tcl',
                   dest='dumptcl',
                   default=False,
                   help='true/false dump a tcl parameterization file')
+parser.add_option('-i','--useIso',
+                  action="store_true",
+                  dest='useIso',
+                  default=False,
+                  help='true/false multiply by iso ratio in tcl file')
+
 (opt, args) = parser.parse_args()
 
 inFileD = opt.inFileD
 inFileF = opt.inFileF
 printoutdir = opt.printoutdir
 dumptcl = opt.dumptcl
+useIso = opt.useIso
 
 if not os.path.exists(printoutdir):
     os.system('mkdir -p %s'%printoutdir)
@@ -64,16 +71,30 @@ for name in hist_names:
     canv = rt.TCanvas(canv_name, canv_name, 900, 600)
     hd = inputFile_d.Get(name)
     hf = inputFile_f.Get(name)
+    test = hf.Integral()
+    if test == 0: continue
 
-    if 'efficiency2D' in name or 'fakerate2D' in name:
+    if 'efficiency2D' in name or 'fakerate2D' in name or 'fakenonisorate2D' in name:
         rt.gStyle.SetPaintTextFormat("1.2f")
         hd.SetStats(rt.kFALSE)
         if 'efficiency' in name: 
             hd.GetZaxis().SetRangeUser(0,1)
             hf.GetZaxis().SetRangeUser(0,1)
+            if hd.GetYaxis().GetBinUpEdge(hd.GetNbinsY()) > 10:
+                hd.GetYaxis().SetRange(1,hd.GetNbinsY()-1)
+                hf.GetYaxis().SetRange(1,hf.GetNbinsY()-1)
+            if hd.GetXaxis().GetBinUpEdge(hd.GetNbinsX()) > 5000:
+                hd.GetXaxis().SetRange(1,hd.GetNbinsX()-1)
+                hf.GetXaxis().SetRange(1,hf.GetNbinsX()-1)
         else: 
             hd.GetZaxis().SetRangeUser(0,0.2)
             hf.GetZaxis().SetRangeUser(0,0.2)
+            if hd.GetYaxis().GetBinUpEdge(hd.GetNbinsY()) > 10:
+                hd.GetYaxis().SetRange(1,hd.GetNbinsY()-1)
+                hf.GetYaxis().SetRange(1,hf.GetNbinsY()-1)
+            if hd.GetXaxis().GetBinUpEdge(hd.GetNbinsX()) > 5000:
+                hd.GetXaxis().SetRange(1,hd.GetNbinsX()-1)
+                hf.GetXaxis().SetRange(1,hf.GetNbinsX()-1)
         hd.Draw("colz texte")
         canv.Print(printoutdir+"/"+canv_name+"_delphes.png")
         hf.SetStats(rt.kFALSE)        
@@ -88,18 +109,47 @@ for name in hist_names:
         hd.SetMarkerStyle(20)
         hd.SetMarkerColor(rt.kRed)
         hd.SetStats(rt.kFALSE)        
-        if 'efficiency' not in name and 'fakerate' not in name and 'ptresponse' not in name:
+        if 'efficiency' not in name and 'fake' not in name and 'nonprompt' not in name and 'ptresponse' not in name:
             hf.Scale(1.0/hf.Integral())
             hd.Scale(1.0/hd.Integral())
 
-        if 'efficiency' in name or 'fakerake' in name:
+        if 'efficiency' in name or 'fake' in name or 'nonprompt' in name:
             hf.SetMaximum(1)
         else:
             hf.SetMaximum(max(hd.GetMaximum(),hf.GetMaximum())*1.1)
-
         hf.SetMinimum(0)
-        hf.Draw()
-        hd.Draw("same")
+
+        if '2D' not in name: 
+            if 'Profile' in hf.ClassName() or 'Profile' in hd.ClassName():
+                try: 
+                    hdProject = hd.ProjectionX('projXD_'+name)
+                    hdProject.SetLineColor(rt.kRed)
+                    hdProject.SetMarkerStyle(20)
+                    hdProject.SetMarkerColor(rt.kRed)
+                    hdProject.SetStats(rt.kFALSE)        
+                except: print('delphes histogram is missing:',name)
+                try: 
+                    hfProject = hf.ProjectionX('projXF_'+name)
+                    hfProject.SetLineColor(rt.kBlue)
+                    hfProject.SetMarkerStyle(21)
+                    hfProject.SetMarkerColor(rt.kBlue)
+                    hfProject.SetStats(rt.kFALSE)
+                except: print('fullsim histogram is missing:',name)
+                hR = rt.TRatioPlot(hdProject, hfProject)
+            else:
+                hR = rt.TRatioPlot(hd, hf)
+            hR.SetH1DrawOpt("P E0")
+            hR.SetGraphDrawOpt("P E0 X0")
+            hR.Draw("P E0")
+            hR.GetLowerRefYaxis().SetTitle("delphes/fullsim")
+            hR.GetLowerRefGraph().SetMaximum(1.5)
+            hR.GetLowerRefGraph().SetMarkerStyle(20)
+            canv.Update()
+        else:
+            hf.Draw()
+            hd.Draw("same")
+
+
         legend = rt.TLegend(.9,.9,.99,.99)
         legend.SetTextSize(0.03)
         legend.SetBorderSize(0) 
@@ -111,31 +161,61 @@ for name in hist_names:
 
 if dumptcl:
     dumpme = ['efficiency2D_looseID','efficiency2D_tightID']
+    photondump = ['nonpromptfakerate2D_looseID', 'nonpromptfakerate2D_tightID','fakerate2D_looseID','fakerate2D_tightID']
+
+    particle = (hist_names[0].split('_')[0]).replace('gen','')
+    if particle == 'photon': dumpme.extend(photondump) 
+
     for dumpname in dumpme:
         quality = dumpname.split('_')[-1]
-        particle = (hist_names[0].split('_')[0]).replace('gen','')
         name = particle+'_'+dumpname
-        print 'useing hist name',name
+        print 'dumping tcl using hist name',name
 
-        useIso = True
-        if particle == 'gamma' or particle == 'jet_': useIso = False;
+        form = 'Efficiency'
+        if 'non' in dumpname: form = 'NonPrompt';
+        elif 'fake' in dumpname: form = 'Fake';
+
+        if particle == 'muon' or particle == 'electron': 
+            useIso = True
+            print 'Forcing useIso = true for muon/electron tcl files'
 
         id2D_f = inputFile_f.Get(name).ProjectionXY("id_"+name)
-        if useIso:
-            iso2D_d = inputFile_d.Get(name.replace('ID','ISOifReco')).ProjectionXY("isoD_"+name)
-            iso2D_f = inputFile_f.Get(name.replace('ID','ISOifReco')).ProjectionXY("isoF_"+name)
+        if useIso: 
+            if particle != 'photon':
+                #(RecoFS eff * IDFS eff)*(IsoFS eff)/(RecoDelphes eff * IsoDelphes eff)
+                iso2D_d = inputFile_d.Get(name.replace('ID','ISO')).ProjectionXY("isoD_"+name) # if removing Reco, add "ifReco" to ID and ISO
+                iso2D_f = inputFile_f.Get(name.replace('ID','ISOifReco')).ProjectionXY("isoF_"+name)
+            else:
+                iso2D_d = inputFile_d.Get(name).ProjectionXY("isoD_"+name)
+                iso2D_f = inputFile_f.Get(name).ProjectionXY("isoF_"+name)
 
-        f = open(printoutdir+'/'+particle+quality+'Efficiency.tcl','w')
-        f.write('## Fullsim Efficiency for '+name+', multiplying ISO Fullsim/Delphes?'+str(useIso)+'\n\n')
-        f.write('set EfficiencyFormula{\n')
+        f = open(printoutdir+'/'+particle+quality+form+'.tcl','w')
+        f.write('## Fullsim Efficiency for '+name+', multiplying ISO(mu/el) or RECO(photon) Fullsim/Delphes? '+str(useIso)+'\n\n')
+
+        if form == 'Efficiency':
+            f.write('set EfficiencyFormula {\n')
+        elif form == 'NonPrompt':
+            f.write('set NonpromptFormula {\n')
+        elif form == 'Fake':
+            f.write('set FakeFormula {\n')
+
+        ptlow = id2D_f.GetXaxis().GetBinLowEdge(1)
+        f.write('\t(pt <= '+str(ptlow)+')*(1.0) +\n')
+
         for ybin in range(0,id2D_f.GetNbinsY()): ## eta
+            isetaOF = False
+
             if id2D_f.GetYaxis().GetBinWidth(ybin+1) == 0: continue
             etalow = id2D_f.GetYaxis().GetBinLowEdge(ybin+1)
             etahigh = id2D_f.GetYaxis().GetBinUpEdge(ybin+1)
+            if etahigh > 10: isetaOF = True
+
             for xbin in range (0,id2D_f.GetNbinsX()): ##pt
+                isptOF = False
                 if id2D_f.GetXaxis().GetBinWidth(xbin+1) == 0: continue
                 ptlow = id2D_f.GetXaxis().GetBinLowEdge(xbin+1)
                 pthigh = id2D_f.GetXaxis().GetBinUpEdge(xbin+1)
+                if pthigh > 9e4: isptOF = True
 
                 ratio = id2D_f.GetBinContent(xbin+1,ybin+1)
                 if useIso: 
@@ -144,8 +224,14 @@ if dumptcl:
                         ratio = ratio * iso2D_f.GetBinContent(xbin+1,ybin+1)/delpheseff
                     else: ratio = ratio * iso2D_f.GetBinContent(xbin+1,ybin+1)
 
-                string = "(abs(eta) > "+str(etalow)+" && abs(eta) <= "+str(etahigh)+") * (pt > "+str(ptlow)+" && pt <= "+str(pthigh)+") * ("+str(ratio)+") + \\"
-                if xbin == id2D_f.GetNbinsX()-1 and ybin == id2D_f.GetNbinsY()-1: string = string[:-3]
+                if isptOF:
+                    if isetaOF: string = "(abs(eta) > "+str(etalow)+") * (pt > "+str(ptlow)+") * ("+str(ratio)+") +"
+                    else: string = "(abs(eta) > "+str(etalow)+" && abs(eta) <= "+str(etahigh)+") * (pt > "+str(ptlow)+") * ("+str(ratio)+") +"
+                else:
+                    if isetaOF: string = "(abs(eta) > "+str(etalow)+") * (pt > "+str(ptlow)+" && pt <= "+str(pthigh)+") * ("+str(ratio)+") +"
+                    else: string = "(abs(eta) > "+str(etalow)+" && abs(eta) <= "+str(etahigh)+") * (pt > "+str(ptlow)+" && pt <= "+str(pthigh)+") * ("+str(ratio)+") +"
+                
+                if xbin == id2D_f.GetNbinsX()-1 and ybin == id2D_f.GetNbinsY()-1: string = string[:-2]
 
                 f.write('\t'+string+'\n')
         f.write('}\n')
