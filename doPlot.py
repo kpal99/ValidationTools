632d73e2d43f195877c10a8d9b5d5c93c0e3d8b1
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2020-11-02 16:09:45 +0100

    added resolution plots

--- doPlot.py
+++ doPlot.py
@@ -1,5 +1,55 @@
 import ROOT as rt
 import os, sys, optparse
+from collections import OrderedDict
+import math
+#____________________________________________________________________________
+def get_mean_and_sigma(theHist, wmin=0.2, wmax=1.8, step=0.001, epsilon=0.007):
+
+    ## rms, signal peak position
+    x0 = theHist.GetXaxis().GetBinCenter(theHist.GetMaximumBin())
+    d  = theHist.GetRMS()
+
+    # now perform gaussian fit in [x_max_sigm, x_max_sigp]
+    f = rt.TF1('gausfit', 'gaus',0.0, 2.0)
+
+    s = 1.0
+    theHist.Fit('gausfit', 'Q', '', x0 - s*d, x0 + s*d)
+
+    mu  = f.GetParameter(1)
+    sig = f.GetParameter(2)
+
+    point = wmin
+    weight = 0.
+    points = [] #vector<pair<double,double> > 
+    thesum = theHist.Integral()
+    for i in range(theHist.GetNbinsX()):
+      weight += theHist.GetBinContent(i)
+      if weight > epsilon:
+        points.append( [theHist.GetBinCenter(i),weight/thesum] )
+    low = wmin
+    high = wmax
+
+    #print points
+
+    width = wmax-wmin
+    for i in range(len(points)):
+      for j in range(i,len(points)):
+        wy = points[j][1] - points[i][1]
+        if abs(wy-0.683) < epsilon:
+
+          wx = points[j][0] - points[i][0]
+          if wx < width:
+            low = points[i][0]
+            high = points[j][0]
+            width=wx
+
+    sig_eff = 0.5*(high-low)
+
+    #print mu,sig, sig_eff
+    return mu,sig, sig_eff
+
+
+#____________________________________________________________________________
 
 rt.gROOT.SetBatch(True) ## avoid figures pop out to screen
 
@@ -45,6 +95,11 @@ if not os.path.exists(printoutdir):
 inputFile_d = rt.TFile.Open(inFileD)
 inputFile_f = rt.TFile.Open(inFileF)
 
+## these dicts contain resolutions to be dumped in tcl format
+mean_and_sigmas_d = OrderedDict()
+mean_and_sigmas_f = OrderedDict()
+
+
 hist_names = []
 # hist_names += [ # select hists to print -- CAN be empty -> will take all 
 # "jet_pt", "jet_eta", "jet_phi", "jet_mass",
@@ -74,6 +129,37 @@ for name in hist_names:
     test = hf.Integral()
     if test == 0: continue
 
+
+    if 'resolution' in name:
+        items = name.split('_')
+        print items
+        
+        colname = items[0]
+        quality = items[1]
+        ptmin = items[4]
+        ptmax = items[5]
+        etamin = items[7].replace('p','.')
+        etamax = items[8].replace('p','.')
+
+        if 'Inf' in ptmax:
+            ptmax = 14000.
+        if 'Inf' in etamax:
+            etamax = 5.
+            
+        etamin = float(etamin)
+        etamax = float(etamax)
+        ptmin = float(ptmin)
+        ptmax = float(ptmax)
+
+        print colname, quality, ptmin, ptmax, etamin, etamax
+
+        ## form input ntuple for mean_and_sigmas dictionary
+        ntup_in = (colname, quality, ptmin, ptmax, etamin, etamax)
+
+        mean_and_sigmas_d[ntup_in] = get_mean_and_sigma(hd, wmin=0.2, wmax=1.8, step=0.001, epsilon=0.007)
+        mean_and_sigmas_f[ntup_in] = get_mean_and_sigma(hf, wmin=0.2, wmax=1.8, step=0.001, epsilon=0.007)
+
+    '''
     if 'efficiency2D' in name or 'fakerate2D' in name or 'fakenonisorate2D' in name:
         rt.gStyle.SetPaintTextFormat("1.2f")
         hd.SetStats(rt.kFALSE)
@@ -110,8 +196,11 @@ for name in hist_names:
         hd.SetMarkerColor(rt.kRed)
         hd.SetStats(rt.kFALSE)        
         if 'efficiency' not in name and 'fake' not in name and 'nonprompt' not in name and 'ptresponse' not in name:
-            hf.Scale(1.0/hf.Integral())
-            hd.Scale(1.0/hd.Integral())
+            
+            if hf.Integral() > 0:
+                hf.Scale(1.0/hf.Integral())
+            if hd.Integral() > 0:
+                hd.Scale(1.0/hd.Integral())
 
         if 'efficiency' in name or 'fake' in name or 'nonprompt' in name:
             hf.SetMaximum(1)
@@ -157,9 +246,11 @@ for name in hist_names:
         legend.AddEntry(hf,"FullSim","l")
         legend.Draw()
         canv.Print(printoutdir+ "/" + canv_name +".png")
-
+   '''
 
 if dumptcl:
+
+    '''
     dumpme = ['efficiency2D_looseID','efficiency2D_tightID']
     photondump = ['nonpromptfakerate2D_looseID', 'nonpromptfakerate2D_tightID','fakerate2D_looseID','fakerate2D_tightID']
 
@@ -236,4 +327,100 @@ if dumptcl:
                 f.write('\t'+string+'\n')
         f.write('}\n')
         f.close()
+    '''
     
+    ## resolution dumps
+    
+    ntup_list = mean_and_sigmas_d.keys()
+    
+    ## order first by collection , then by quality, then by eta min, then by ptmin 
+    sorted_ntup_list = sorted(ntup_list, key=lambda v: (v[0], v[1], v[4], v[2]))
+    
+
+    old_coll = ''
+    old_quality = ''
+    old_etamin = -1
+    old_etamax = -1
+    old_ptmin = -1
+    old_ptmax = -1
+    
+    lines_scale = dict()
+    lines_reso = dict()
+    
+    for ntup_in in sorted_ntup_list:
+        #print ntup_in, mean_and_sigmas_d[ntup_in], mean_and_sigmas_f[ntup_in]
+
+        coll    = ntup_in[0]
+        quality = ntup_in[1]
+        ptmin   = ntup_in[2]
+        ptmax   = ntup_in[3]
+        etamin  = ntup_in[4]
+        etamax  = ntup_in[5]
+        
+        if coll != old_coll:
+            old_coll = coll
+            
+        if quality != old_quality:
+
+            old_quality = quality
+            lines_scale[(coll,quality)] = []
+            lines_scale[(coll,quality)].append('### {} {} momentum scale\n'.format(coll, quality))
+            lines_scale[(coll,quality)].append('set ScaleFormula {\n')
+
+            lines_reso[(coll,quality)] = []
+            lines_reso[(coll,quality)].append('### {} {} momentum resolution\n'.format(coll, quality))
+            lines_reso[(coll,quality)].append('set ResolutionFormula {\n')
+
+
+        ## compute values to write in delphes card
+        
+        mu_d = mean_and_sigmas_d[ntup_in][0]
+        mu_f = mean_and_sigmas_f[ntup_in][0]
+        
+        ## 1 - is gaussian width and 2 - is effective width
+        sigma_d = mean_and_sigmas_f[ntup_in][2]
+        sigma_f = mean_and_sigmas_d[ntup_in][2]
+        
+        
+        scale = 1.
+        if mu_d > 0.:
+            scale = mu_f / mu_d
+        
+        ## delphes resolution when morphed to full sim scale
+        
+        sigmap_d = sigma_d 
+        if mu_f > 0.:
+            sigmap_d = sigma_d*scale
+            
+        sigma_smear = 1.e-04
+        if sigma_f**2 > sigmap_d**2: 
+            sigma_smear = math.sqrt(sigma_f**2 - sigmap_d**2)
+
+
+        #print '{}, {}, {}, {}, {}, {}'.format(mu_f, mu_d, sigma_f, sigma_d, sigmap_d, sigma_smear)
+
+        lines_scale[(coll,quality)].append('   (abs(eta) > {:.1f} && abs(eta) <= {:.1f}) * (pt > {:.1f} && pt <= {:.1f}) * ({:.3f}) +\n'.format(etamin, etamax, ptmin, ptmax, scale))
+        lines_reso[(coll,quality)].append('   (abs(eta) > {:.1f} && abs(eta) <= {:.1f}) * (pt > {:.1f} && pt <= {:.1f}) * ({:.4f}) +\n'.format(etamin, etamax, ptmin, ptmax, sigma_smear))
+
+
+    ## dump scale tcl file    
+    for k, v in lines_scale.iteritems():
+        
+        dump = v
+        dump.append('}\n')
+
+        F = open("{}/{}_{}_scale.tcl".format(printoutdir,k[0],k[1]), "w")
+        F.writelines(v)
+        F.close()
+
+
+    ## dump scale reso file    
+    for k, v in lines_reso.iteritems():
+        
+        dump = v
+        dump.append('}\n')
+
+        F = open("{}/{}_{}_reso.tcl".format(printoutdir,k[0],k[1]), "w")
+        F.writelines(v)
+        F.close()
+
