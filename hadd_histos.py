793f18895ff7124b65f53a8e1bf17e9424f7fdf3
Michele Selvaggi <michele.selvaggi@uclouvain.be>
2021-04-20 18:32:03 +0200

    fixed excessively long cmd hadd

--- hadd_histos.py
+++ hadd_histos.py
@@ -8,28 +8,44 @@ debug=False
 
 sim = sys.argv[1]
 
-macro_samples={
-    'ELMu_113X':'113X_DYToLL',
+macro_samples_fullsim={
+    'ELMu_113X':'113X_DYToLL:Zprime',
     'Photon_113X':'113X_GluGluToHHTo2B2G:113X_GluGluHToGG',
     #'QCD_112X':'112X_QCD',
     'QCD_113X':'113X_QCD',
     'TauTag_112X':'112X_GluGluToHHTo2B2Tau:112X_VBFHToTauTau:112X_GluGluHToTauTau:112X_TT_TuneCP5:112X_QCD',
     #'TauTag':'VBFHToTauTau',
-    'BTag_112X':'112X_TT_TuneCP5:112X_GluGluToHHTo2B2Tau:112X_QCD',
+    #'BTag_112X':'112X_TT_TuneCP5:112X_GluGluToHHTo2B2Tau:112X_QCD',
+    'BTag_112X':'112X_TT_TuneCP5',
 }
+
+macro_samples_delphes={
+    'ELMu':'DYToLL:Zprime',
+    'Photon':'GluGluToHHTo2B2G:GluGluHToGG',
+    'QCD':'QCD',
+    #'TauTag':'GluGluToHHTo2B2Tau:VBFHToTauTau:GluGluHToTauTau:TT_TuneCP5:112X_QCD',
+    'TauTag':'VBFHToTauTau:GluGluToHHTo2B2Tau:VBFHToTauTau:GluGluHToTauTau:TT_TuneCP5:QCD:Zprime',
+    'BTag':'TT_TuneCP5:GluGluToHHTo2B2Tau:QCD',
+    #'BTag':'TT_TuneCP5',
+}
+
+
+macro_samples=macro_samples_fullsim
 
 
 eospath='/eos/cms/store/group/upgrade/RTB/ValidationHistos/fullsim_Iter6/'
 name='HistosFS'
 
-delphes_version='343pre10'
-card_version='v11_dummy'
+delphes_version='343pre11'
+card_version='v12a'
+
 
 
 if 'delphes' in sim:
     #eospath='/eos/cms/store/group/upgrade/RTB/ValidationHistos/delphes343pre07_v09/'
     eospath='/eos/cms/store/group/upgrade/RTB/ValidationHistos/delphes{}_{}/'.format(delphes_version,card_version)
     name='HistosDELPHES'
+    macro_samples=macro_samples_delphes
 
 haddpath=eospath
 #haddpath='./'
@@ -40,34 +56,48 @@ histo_dirs = histo_dirs[1:]
 
 #print histo_dirs
 
-for key in macro_samples.keys():
+working_dir=os.getcwd()
 
-    #print '    '
-    print 'hadding {} sample'.format(key)
 
-    #if key != 'BTag': continue
-    hadd_file='{}{}_{}.root'.format(haddpath, name, key)
-    #print '    '
-    #print 'hadding   ', hadd_file
-    #print '    '
+with open("hadd_cmds.sh", "w") as text_file:
 
-    hadd_cmd='hadd -f {}'.format(hadd_file)
+    for key in macro_samples.keys():
+
+        #print '    '
+        print 'hadding {} sample'.format(key)
+
+        if key != 'BTag_112X': continue
+        hadd_file='{}{}_{}.root'.format(haddpath, name, key)
+        #print '    '
+        #print 'hadding   ', hadd_file
+        #print '    '
+
+        hadd_cmd='hadd -k -f {}'.format(hadd_file)
+
+        for proc_str in macro_samples[key].split(':'):
+            print '  subprocess:   ', proc_str
+
+            for dire in histo_dirs:
+                #print name, dire            
+                if  proc_str in dire:
+                    print '    ', dire
+
+                    for root, dirs, files in os.walk(dire):
+                        for filename in files:
+                           abs_fname = '{}/{}'.format(dire,filename)
+                           print '                          ', abs_fname, '   ', os.stat(abs_fname).st_size == 0
+
+                           ## quickly check whether file is not empty
+                           if os.stat(abs_fname).st_size > 0:
+                               hadd_cmd += ' {}'.format(abs_fname)
     
-    for proc_str in macro_samples[key].split(':'):
-        print '  subprocess:   ', proc_str
-        
-        for dire in histo_dirs:
-            #print name, dire            
-            if  proc_str in dire:
-                print '    ', dire
+    hadd_cmd += '\n' 
+    hadd_cmd += '\n' 
 
-                for root, dirs, files in os.walk(dire):
-                    for filename in files:
-                       abs_fname = '{}/{}'.format(dire,filename)
-                       #print '                          ', abs_fname
-
-                       hadd_cmd += ' {}'.format(abs_fname)
-    
-    #print hadd_cmd
     if not debug:
-        os.system(hadd_cmd)
+        text_file.write(hadd_cmd)
+
+if not debug:
+    os.chdir(working_dir)  
+    os.system('pwd')  
+    os.system('source {}/hadd_cmds.sh'.format(working_dir))
