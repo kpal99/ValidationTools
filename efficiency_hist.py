a8f6e706a639ddbc8535174bed74c50fc62e4fce
Kuldeep Pal <kpal@cern.ch>
2022-03-17 04:19:50 -0500

    Variable bin and added pt,relsio 2D histogram

--- efficiency_hist.py
+++ efficiency_hist.py
@@ -2,6 +2,7 @@
 import ROOT
 #from __future__ import print_function
 from bin.NtupleDataFormat import Ntuple
+from array import array
 import sys
 import os
 
@@ -41,14 +42,18 @@ def createHist(varname,minval=0, maxval=501):
     h.Sumw2()
     return h
 
-def globalWeight(inDir, in_str):
-    inFile1 = inDir + "genweight/" + in_str
+def createHistVarBin(varname,minval=0, maxval=501):
+    if "Tight" in varname:
+        varBin = [50, 100, 150, 200, 250, 300, 400, 500, 650, 800]
+        h = ROOT.TH1D(varname, varname, len(varBin)-1, array('d',varBin))
+    elif "jetspuppi_pt" in varname:
+        varBin = [0, 50, 100, 150, 200, 250, 300, 400, 500, 650, 800, 1000, 1200]
+        h = ROOT.TH1D(varname, varname, len(varBin)-1, array('d',varBin))
+    else:
+        h = ROOT.TH1D(varname,varname,binval,minval,maxval)
 
-    ntuple1 = Ntuple(inFile1)
-    gw = 0
-    for event in ntuple1:
-        gw += event.genweight()
-    return gw
+    h.Sumw2()
+    return h
 
 def main():
     if len(sys.argv) != 2:
@@ -66,16 +71,21 @@ def main():
 
 
 # creating very many histrograms
-    hists["TightElectrons_pt"] = createHist("TightElectrons_pt",50,1050)
-    hists["TightMuons_pt"] = createHist("TightMuons_pt",50,1050)
-    hists["jetspuppi_pt"] = createHist("jetspuppi_pt",0,2000)
+    hists["TightElectrons_pt"] = createHistVarBin("TightElectrons_pt",50,1050)
+    hists["TightMuons_pt"] = createHistVarBin("TightMuons_pt",50,1050)
+    hists["jetspuppi_pt"] = createHistVarBin("jetspuppi_pt",0,2000)
+    hists["TightElectrons_pt_cut"] = createHistVarBin("TightElectrons_pt_cut",50,1050)
+    hists["TightMuons_pt_cut"] = createHistVarBin("TightMuons_pt_cut",50,1050)
+    hists["jetspuppi_pt_cut"] = createHistVarBin("jetspuppi_pt_cut",0,2000)
+
     hists["Elec_reliso_met"] = ROOT.TH2D("Elec_reliso_met","Elec_reliso_met", 40, 50., 500., 40, 0, 2.)
     hists["Muon_reliso_met"] = ROOT.TH2D("Muon_reliso_met","Muon_reliso_met", 40, 50., 500., 40, 0, 2.)
-    hists["TightElectrons_pt_cut"] = createHist("TightElectrons_pt_cut",50,1050)
-    hists["TightMuons_pt_cut"] = createHist("TightMuons_pt_cut",50,1050)
-    hists["jetspuppi_pt_cut"] = createHist("jetspuppi_pt_cut",0,2000)
+    hists["Elec_reliso_pt"] = ROOT.TH2D("Elec_reliso_pt","Elec_reliso_pt", 40, 50., 500., 40, 0, 2.)
+    hists["Muon_reliso_pt"] = ROOT.TH2D("Muon_reliso_pt","Muon_reliso_pt", 40, 50., 500., 40, 0, 2.)
     hists["Elec_reliso_met_cut"] = ROOT.TH2D("Elec_reliso_met_cut","Elec_reliso_met_cut", 40, 50., 500., 40, 0, 2.)
     hists["Muon_reliso_met_cut"] = ROOT.TH2D("Muon_reliso_met_cut","Muon_reliso_met_cut", 40, 50., 500., 40, 0, 2.)
+    hists["Elec_reliso_pt_cut"] = ROOT.TH2D("Elec_reliso_pt_cut","Elec_reliso_pt_cut", 40, 50., 500., 40, 0, 0.2)
+    hists["Muon_reliso_pt_cut"] = ROOT.TH2D("Muon_reliso_pt_cut","Muon_reliso_pt_cut", 40, 50., 500., 40, 0, 0.2)
 
 # iterating through the all events; if value of maxEvents is zero.
     for event in ntuple:
@@ -96,17 +106,21 @@ def main():
             if item.idpass() >= 4 and item.pt() > 60 and abs(item.eta()) < 2.5:
                 hists["TightElectrons_pt"].Fill(item.pt())
                 hists["Elec_reliso_met"].Fill(met, item.reliso())
+                hists["Elec_reliso_pt"].Fill(item.pt(), item.reliso())
                 if item.isopass() >= 4:
                     hists["TightElectrons_pt_cut"].Fill(item.pt())
                     hists["Elec_reliso_met_cut"].Fill(met, item.reliso())
+                    hists["Elec_reliso_pt_cut"].Fill(item.pt(), item.reliso())
                 break
         for item in event.muons():
             if item.idpass() >= 4 and item.pt() > 60 and abs(item.eta()) < 2.4:
                 hists["TightMuons_pt"].Fill(item.pt())
                 hists["Muon_reliso_met"].Fill(met, item.reliso())
+                hists["Muon_reliso_pt"].Fill(item.pt(), item.reliso())
                 if item.isopass() >= 4:
                     hists["TightMuons_pt_cut"].Fill(item.pt())
                     hists["Muon_reliso_met_cut"].Fill(met, item.reliso())
+                    hists["Muon_reliso_pt_cut"].Fill(item.pt(), item.reliso())
                 break
 
     outFile.Write()
