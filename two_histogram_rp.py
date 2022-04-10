545988b48b3630a11e2fa21708a2ea1183e07d55
Kuldeep Pal <kpal@cern.ch>
2022-04-10 12:04:46 -0500

    edited two_histogram_rp and canvas_eff_plots

--- two_histogram_rp.py
+++ two_histogram_rp.py
@@ -24,14 +24,14 @@ for key in keys:
 
 outputDir = sys.argv[1].split('.root')[0]
 
-tex1 = ROOT.TLatex(0.10, 0.96, "#bf{CMS} #it{Phase-2 Simulation Premilinary}")
+tex1 = ROOT.TLatex(0.10, 0.97, "#bf{CMS} #it{Phase-2 Simulation Premilinary}")
 tex1.SetNDC()
 tex1.SetTextAlign(13)
 tex1.SetTextFont(42)
 tex1.SetTextSize(0.03)
 tex1.SetLineWidth(2)
 
-tex2 = ROOT.TLatex(0.68, 0.96, "3000 fb^{-1} (14 TeV)")
+tex2 = ROOT.TLatex(0.69, 0.97, "3000 fb^{-1} (14 TeV)")
 tex2.SetNDC()
 tex2.SetTextAlign(13)
 tex2.SetTextFont(42)
@@ -41,15 +41,21 @@ tex2.SetLineWidth(2)
 for key in hists.keys():
     if "_pt_cut" in key and "Tight" in key:
         canvas = ROOT.TCanvas('canvas','',600,600)
-        canvas.SetLogy()
 
         key_divide = key.split("_cut")[0] + key.split("_cut")[1]
 
-        rp = ROOT.TRatioPlot(hists[key], hists[key_divide])
+        canvas.cd()
+        pad1 = ROOT.TPad("pad1","pad1",0,0.4,1,1)
+        pad1.SetLogy()
+        pad1.SetBottomMargin(0)
+        pad1.Draw()
+        pad1.cd()
         hists[key].SetLineColor(1)
         hists[key].SetLineWidth(2)
         hists[key].SetTitle("")
-        hists[key].GetXaxis().SetTitle("P_{T} [GeV]")
+        hists[key].GetXaxis().SetLabelSize(0.05)
+        hists[key].GetYaxis().SetLabelSize(0.05)
+        hists[key].GetYaxis().SetTitleSize(0.05)
         hists[key].GetYaxis().SetTitle("events/bin")
 
         maximum = 0
@@ -58,11 +64,35 @@ for key in hists.keys():
             if maximum < binContent:
                 maximum = binContent
 
-        rp.Draw()
-        rp.GetUpperRefYaxis().SetRangeUser(0, 1)
-        rp.GetUpperRefYaxis().SetRangeUser(1, 2 * maximum)
+        hists[key].SetMaximum(5 * maximum)
+        hists[key].SetMinimum(1)
+        hists[key].Draw("hist")
+        hists[key_divide].Draw("E same")
 
-        legend1 = ROOT.TLegend(0.55,0.90,0.89,0.80)
+        canvas.cd()
+        pad2 = ROOT.TPad("pad2","pad2",0,0,1,0.4)
+        pad2.SetTopMargin(0.0)
+        pad2.SetBottomMargin(0.3)
+        pad2.SetGrid()
+        pad2.Draw()
+        pad2.cd()
+        hist = hists[key].Clone()
+        hist.Reset()
+        hist.Divide(hists[key],hists[key_divide],1,1,"B")
+        hist.SetMinimum(0)
+        hist.SetMaximum(1.1)
+        hist.GetXaxis().SetLabelSize(0.07)
+        hist.GetYaxis().SetLabelSize(0.07)
+        hist.GetXaxis().SetTitleSize(0.07)
+        hist.SetTitle("")
+        hist.GetXaxis().SetTitle("P_{T} [GeV]")
+        hist.GetYaxis().SetTitle("")
+        hist.Draw("E")
+
+        canvas.cd()
+
+        legend1 = ROOT.TLegend(0.15,0.92,0.9,0.89)
+        legend1.SetNColumns(2)
         legend1.SetBorderSize(0)
         legend1.AddEntry(hists[key],"w/o reliso cut", "l")
         legend1.AddEntry(hists[key_divide],"w reliso 0.1(0.15) cut", "l")
@@ -76,15 +106,21 @@ for key in hists.keys():
 
     if "jetspuppi_pt_cut_" in key:
         canvas = ROOT.TCanvas('canvas','',600,600)
-        canvas.SetLogy()
 
         key_divide = key.split("_cut")[0] + key.split("_cut")[1]
 
-        rp = ROOT.TRatioPlot(hists[key], hists[key_divide])
+        canvas.cd()
+        pad1 = ROOT.TPad("pad1","pad1",0,0.4,1,1)
+        pad1.SetLogy()
+        pad1.SetBottomMargin(0)
+        pad1.Draw()
+        pad1.cd()
         hists[key].SetLineColor(1)
         hists[key].SetLineWidth(2)
         hists[key].SetTitle("")
-        hists[key].GetXaxis().SetTitle("P_{T} [GeV]")
+        hists[key].GetXaxis().SetLabelSize(0.05)
+        hists[key].GetYaxis().SetLabelSize(0.05)
+        hists[key].GetYaxis().SetTitleSize(0.05)
         hists[key].GetYaxis().SetTitle("events/bin")
 
         maximum = 0
@@ -93,11 +129,35 @@ for key in hists.keys():
             if maximum < binContent:
                 maximum = binContent
 
-        rp.Draw()
-        rp.GetUpperRefYaxis().SetRangeUser(0, 1)
-        rp.GetUpperRefYaxis().SetRangeUser(1, 2 * maximum)
+        hists[key].SetMaximum(5 * maximum)
+        hists[key].SetMinimum(1)
+        hists[key].Draw("hist")
+        hists[key_divide].Draw("E same")
 
-        legend1 = ROOT.TLegend(0.55,0.45,0.89,0.35)
+        canvas.cd()
+        pad2 = ROOT.TPad("pad2","pad2",0,0,1,0.4)
+        pad2.SetTopMargin(0.0)
+        pad2.SetBottomMargin(0.3)
+        pad2.SetGrid()
+        pad2.Draw()
+        pad2.cd()
+        hist = hists[key].Clone()
+        hist.Reset()
+        hist.Divide(hists[key],hists[key_divide],1,1,"B")
+        hist.SetMinimum(0)
+        hist.SetMaximum(1.1)
+        hist.GetXaxis().SetLabelSize(0.07)
+        hist.GetYaxis().SetLabelSize(0.07)
+        hist.GetXaxis().SetTitleSize(0.07)
+        hist.SetTitle("")
+        hist.GetXaxis().SetTitle("P_{T} [GeV]")
+        hist.GetYaxis().SetTitle("")
+        hist.Draw("E")
+
+        canvas.cd()
+
+        legend1 = ROOT.TLegend(0.15,0.92,0.9,0.89)
+        legend1.SetNColumns(2)
         legend1.SetBorderSize(0)
         legend1.AddEntry(hists[key],"w/o reliso cut", "l")
         legend1.AddEntry(hists[key_divide],"w reliso 0.1(0.15) cut", "l")
