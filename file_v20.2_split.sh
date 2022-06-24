5c20737cc2af06eb9c7807096d6696a41e66598c
Kuldeep Pal <kpal@cern.ch>
2022-06-24 02:13:36 -0500

    added file_v20.2_split.sh

---
+++ file_v20.2_split.sh
@@ -0,0 +1,72 @@
+echo "=== h2b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h2b/rootPlots_st/qcd.root
+echo "=== h1b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/h1b/rootPlots_st/qcd.root
+echo "=== w3b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w3b/rootPlots_st/qcd.root
+echo "=== w2b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w2b/rootPlots_st/qcd.root
+echo "=== w1b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/w1b/rootPlots_st/qcd.root
+echo "=== _3b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_3b/rootPlots_st/qcd.root
+echo "=== _2b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_2b/rootPlots_st/qcd.root
+echo "=== _1b started ==="
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/TT_M1000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/TT_M1500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/TT_M2000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/TT_M2500.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/TT_M3000.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/ew.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/top.root
+python event_count.py /eos/uscms/store/user/kpal/trimmed_files_v20.2/_1b/rootPlots_st/qcd.root
